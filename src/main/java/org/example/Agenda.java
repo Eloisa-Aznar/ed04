e5a9bffa58ba4b1dbfbb809dbd95d663d852d651
Eloisa-Aznar <1801581@alu.murciaeduca.es>
2025-03-11 22:06:16 +0100

    Cambio de nombre la clase Contacto por Persona

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Agenda {
-    private List<Contacto> contacts; // Lista de Contacto
+    private List<Persona> contacts; // Lista de Persona
 
     /**
      * Constructor de la agenda.
@@ -21,7 +21,7 @@ public class Agenda {
      */
     public void addContact(String name, String phone) {
         boolean exists = false;
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 exists = true;
                 c.getPhones().add(phone);
@@ -30,7 +30,7 @@ public class Agenda {
         }
 
         if (!exists) {
-            Contacto newContact = new Contacto(name, phone);
+            Persona newContact = new Persona(name, phone);
             contacts.add(newContact);
         }
     }
@@ -40,10 +40,10 @@ public class Agenda {
      * @param name nombre del contacto.
      */
     public void removeContact(String name) {
-        Iterator<Contacto> it = contacts.iterator();
+        Iterator<Persona> it = contacts.iterator();
 
         while (it.hasNext()) {
-            Contacto c = it.next();
+            Persona c = it.next();
 
             if (c.getName().equalsIgnoreCase(name)) {
                 it.remove();
@@ -58,7 +58,7 @@ public class Agenda {
      * @param newPhone número de teléfono nuevo.
      */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 List<String> phones = c.getPhones();
 
@@ -75,7 +75,7 @@ public class Agenda {
      * Getter
      * @return devuelve los contactos de la agenda.
      */
-    public List<Contacto> getContacts() {
+    public List<Persona> getContacts() {
         return this.contacts;
     }
 }
